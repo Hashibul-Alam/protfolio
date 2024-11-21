c9f3d230537f54a32b465b9deec8aa1b08185233
sha2311 <sha31417@gmail.com>
2024-11-21 16:26:45 +0600

    new add

--- src/constants/index.js
+++ src/constants/index.js
@@ -70,7 +70,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "767 Fifth Avenue, Dhaka, Bangladesh ",
+  phoneNo: "+880 01949200000",
+  email: "sha3141@gmail.com",
 };
